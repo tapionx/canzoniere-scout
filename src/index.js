6f3ae947c833136233c3c7f5986196450d3af231
Riccardo Serafini <tapion@tapion.it>
2024-12-08 22:44:05 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let pageHeight = 1530;
     let pageWidth = 1100;
     let ratio = 1.41; // height 210mm / width 148.5mm
-    let fontStep = 0.5;
+    let fontStep = 0.2;
     const songs = document.querySelectorAll('.song');
     songs.forEach(song => {
         console.log(song);
@@ -13,24 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
             while (song.clientHeight < pageHeight) {
                 fontSize += fontStep;
                 lyrics.style.fontSize = fontSize + 'px';
-                console.log("A", song.clientHeight, pageHeight, fontSize);
             }
             while (song.clientHeight > pageHeight) {
                 fontSize -= fontStep;
                 lyrics.style.fontSize = fontSize + 'px';
-                console.log("B", song.clientHeight, pageHeight, fontSize);
             }
             if (song.clientWidth < pageWidth / 2) {
                 lyrics.classList.add('columns');
                 while (song.clientWidth < pageWidth) {
                     fontSize += fontStep;
                     lyrics.style.fontSize = fontSize + 'px';
-                    console.log("1C", song.clientWidth, pageWidth, fontSize);
                 }
                 while (song.clientWidth > pageWidth) {
                     fontSize -= fontStep;
                     lyrics.style.fontSize = fontSize + 'px';
-                    console.log("1D", song.clientWidth, pageWidth, fontSize);
                 }
             }
         } else {
@@ -38,12 +34,10 @@ document.addEventListener("DOMContentLoaded", function() {
             while (song.clientWidth < pageWidth) {
                 fontSize += fontStep;
                 lyrics.style.fontSize = fontSize + 'px';
-                console.log("C", song.clientWidth, pageWidth, fontSize);
             }
             while (song.clientWidth > pageWidth) {
                 fontSize -= fontStep;
                 lyrics.style.fontSize = fontSize + 'px';
-                console.log("D", song.clientWidth, pageWidth, fontSize);
             }
         }
     });
