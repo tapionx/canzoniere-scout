6c11c6afe8b01916e4fc6371cba3368a91ac1b25
Riccardo Serafini <tapion@tapion.it>
2025-01-04 12:36:48 +0100

    update some songs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let pageHeight = 1530;
+    let pageHeight = 1500;
     let pageWidth = 1100;
     let ratio = 1.41; // height 210mm / width 148.5mm
     let fontStep = 0.2;
@@ -9,16 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
         let fontSize = parseFloat(window.getComputedStyle(song).fontSize); 
         const lyrics = song.querySelector('.lyrics');
         // first make sure height is in one page
-        while (song.clientHeight > pageHeight || song.clientWidth > pageWidth) {
+        while (song.clientHeight > pageHeight || lyrics.clientWidth > pageWidth) {
             fontSize -= fontStep;
             lyrics.style.fontSize = fontSize + 'px';
         }
         // now we can calculate if it's worth to use columns
-        if (song.clientWidth < pageWidth / 2) {
+        if ((lyrics.clientWidth < pageWidth / 2) && (song.clientHeight > pageHeight * 0.5)) {
             lyrics.classList.add('columns');
         }
         // now we can increase until the limit
-        while (song.clientHeight < pageHeight && song.clientWidth < pageWidth) {
+        while (song.clientHeight < pageHeight && lyrics.clientWidth < pageWidth) {
             fontSize += fontStep;
             lyrics.style.fontSize = fontSize + 'px';
         }
